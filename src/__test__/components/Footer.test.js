e9529ee0d1ed512f9ebf31b429e47759010922ef
luiznaiper <luiz_naiper@hotmail.com>
2022-09-19 08:28:51 -0500

    :white_check_mark: Create test snapshots for UI

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-  const footer = shallow(<Footer />);
+  const footer = mount(<Footer />);
   test('Render of the Footer component', () => {
     expect(footer.length).toEqual(1);
   });
   test('Title render', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Check UI of the Footer component', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
